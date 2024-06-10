f0edeef95fef23413bf94c79e97b2e77d93d4857
Sunti <saknsaza87@gmail.com>
2024-06-10 16:08:50 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -7,31 +7,11 @@ import { useState } from "react";
 
 
 export default function Home() {
-  // Mock data for checkboxes
-  const CheckBoxData = [{ id: 1, value: "แซนวิช", checked: false }, { id: 2, value: "แซนวิช2", checked: false }, { id: 3, value: "แซนวิช3", checked: false }];
-  const [checkedItems, setCheckedItems] = useState(CheckBoxData);
-  const handleCheckboxChange = (id, isChecked) => {
-    console.log({id})
-    setCheckedItems(prevState =>
-      prevState.map(item =>
-        item.id === id ? { ...item, checked: isChecked } : item
-      )
-    );
 
-  };
-  console.log({ checkedItems })
 
   return (
     <>
-      {checkedItems.map((item) => (
-        <CheckBoxCustom
-          key={item.id}
-          item={item}
-          isChecked={item.checked}
-          onCheckboxChange={(isChecked) => handleCheckboxChange(item.id, isChecked)}
-          text={item.value}
-        />
-      ))}
+      <div>Home</div>
     </>
   );
 }
