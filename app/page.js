a4b27fa4c795afeaa4e1cc2aec7254f529941a29
Sunti <saknsaza87@gmail.com>
2024-06-11 13:54:40 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -25,10 +25,8 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
       </Head>
       <HeaderOne CartCount={10} whenClickBulgur={ClickBulgur} />
-
       <SelectionTopHome />
       {/* <Selectbox type="success" listData={listData}/> */}
-
       <SectionBottom />
       <div className={"absolute top-0 z-50"}>
         <SideNavBar isOpen={isOpenBulgur} Close={setIsOpenBulgur} />
