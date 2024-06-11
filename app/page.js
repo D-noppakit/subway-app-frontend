9cf2cf1ac27d564248dbf5bd50da280d9a19809b
noppakit dononbout <d.noppakit@gmail.com>
2024-06-11 12:11:08 +0700

    update

--- app/page.js
+++ app/page.js
@@ -1,20 +1,28 @@
 
 "use client"
 import Image from "next/image";
-import ButtonOrange from "@/components/Button/ButtonOrange"
+import ButtonCustom from "@/components/Button/ButtonCustom"
 import HeaderOne from "@/components/HeaderOne";
 import SelectionTopHome from "@/components/SelectionTopHome";
 import SectionBottom from "@/components/HomeComponents/SectionBottom";
 import Head from "next/head";
+import Selectbox from "@/components/subwaytextbox"
 
 export default function Home() {
+  let listData = [
+    {id:"1",value:"us"},
+    {id:"2",value:"en"}
+  ]
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
       </Head>
       <HeaderOne CartCount={10} />
+
       <SelectionTopHome />
+      {/* <Selectbox type="success" listData={listData}/> */}
+
       <SectionBottom />
     </>
   );
--- components/subwaytextbox.js
+++ components/subwaytextbox.js
@@ -1,11 +1,45 @@
 
-import * as React from 'react';
-export default function subwaytextbox({type}) {
-    console.log("typeeee",type)
+import * as React from "react";
+import "@/app/globals.css";
+
+export default function subwaytextbox({
+  type,
+  defaultValue ,
+  listData = [],
+  errorMsg = "Error message"
+}) {
   return (
-    <div>
-      <div></div>
-       {/* <Button>Primary</Button> */}
+    <div class="max-w-2xl mx-auto">
+      <div class="custom-select">
+        <select
+          id=""
+          className={
+            "bg-gray-50 border border-gray-300 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 rounded-full " +
+            type
+          }
+          class=""
+          style={{ paddingRight: "30px", width: "240px", height: "48px" }}
+          disabled = {type == 'disabled' ?true:null}
+        >
+          {defaultValue &&
+            <option id={defaultValue}>
+              {defaultValue}
+            </option>}
+          {listData.map(elm => {
+            return (
+              <option id={elm.id}>
+                {elm.value}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+      {errorMsg &&
+        type == "error" &&
+        <div className={"error-msg"}>Error message</div>}
+      {errorMsg &&
+        type == "success" &&
+        <div className={"success-msg"}>success message</div>}
     </div>
   );
 }
