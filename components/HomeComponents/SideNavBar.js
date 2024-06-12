5074d98704573d44754997bafce8c610b2e5f3ae
Sunti <saknsaza87@gmail.com>
2024-06-12 22:47:07 +0700

    reword

--- components/HomeComponents/SideNavBar.js
+++ components/HomeComponents/SideNavBar.js
@@ -10,8 +10,7 @@ import PersonSetting from "@/public/imgs/user-setting-bolt-nut.png"
 import QAimg from "@/public/imgs/QAimg.png"
 import CardHeaderGreen from "@/components/Card/CardHeaderGreen"
 import ButtonCustom from '../Button/ButtonCustom'
-import ButtonWhiteBorderGray from "@/components/Button/ButtonWhiteBorderGray"
-
+import  ButtonMenu  from '@/components/Button/ButtonMenu';
 import store from '@/lib/store';
 
 export default function SideNavBar({ isOpen = false, Close = () => console.log("Close") }) {
@@ -60,19 +59,3 @@ export default function SideNavBar({ isOpen = false, Close = () => console.log("
     );
 }
 
-
-function ButtonMenu({ text, img }) {
-    return (
-        <div className='mt-3 rounded-lg flex  flex-row justify-between items-center h-[56px] w-full bg-white ps-[12px] pe-[12px] pt-[16px] pb-[16px]'>
-            <div className='flex'>
-                <div className={"w-50px pe-2 flex justify-center items-center"}>
-                    <Image src={img} width={25} height={25} alt='arrow' />
-                </div>
-                <div className='flex justify-center items-center' >{text}</div>
-            </div>
-            <div>
-                <Image src={"/imgs/arrow-green.png"} width={15} height={15} alt='arrow' />
-            </div>
-        </div>
-    )
-}
--- components/HomeComponents/SubwayRewards.js
+++ components/HomeComponents/SubwayRewards.js
@@ -1,14 +1,21 @@
 import Image from 'next/image'
 import React, { lazy } from 'react'
 import subwayLogoWhite from '@/public/imgs/subway-logo-white.png'
-export default function SubwayRewards() {
+export default function SubwayRewards({ Click = () => console.log("click reward"), point, text }) {
     return (
-        <div className="w-full h-[75px] bg-[#97D700] absolute bottom-[0vh] rounded-t-[32px]">
-            <div>
-                <Image src={subwayLogoWhite}width={50} height={50}  alt='img'/>
-            </div>
-            <div>
+        <div className="w-full h-[75px] bg-[#97D700] absolute bottom-[0vh] rounded-t-[32px]" onClick={Click}>
+            <div className='flex h-full p-3 w-full'>
 
+                <Image src={subwayLogoWhite} width={45} height={45} style={{ objectFit: "contain" }} alt='img' />
+                <div className='flex w-full flex-col ps-4'>
+                    <div className='text-[#008938] w-full font-[700] text-[16px] flex'>
+                        <spam>ซับเวย์ รีวอร์ด</spam>
+                        <div className='flex justify-center items-center ps-2'>
+                            <Image src={"/imgs/arrow-green.png"} width={13} height={13} alt='arrow' />
+                        </div>
+                    </div>
+                    <div className='text-[#00491E] w-full font-[500] text-[12px]'>ปลดล็อคสิทธิประโยชน์อีกเพียบด้วยสมาชิก ซับเวย์ รีวอร์ด</div>
+                </div>
             </div>
         </div>
     )
