f0e7392c9b31415cbecda6b8d63f0716e56d4575
Sunti <saknsaza87@gmail.com>
2024-06-12 13:53:32 +0700

    update

--- components/HomeComponents/SubwayRewards.js
+++ components/HomeComponents/SubwayRewards.js
@@ -1,7 +1,15 @@
-import React from 'react'
-
+import Image from 'next/image'
+import React, { lazy } from 'react'
+import subwayLogoWhite from '@/public/imgs/subway-logo-white.png'
 export default function SubwayRewards() {
     return (
-        <div className="w-full h-[75px] bg-[#97D700] absolute bottom-[0vh] rounded-t-[32px]"></div>
+        <div className="w-full h-[75px] bg-[#97D700] absolute bottom-[0vh] rounded-t-[32px]">
+            <div>
+                <Image src={subwayLogoWhite}width={50} height={50}  alt='img'/>
+            </div>
+            <div>
+
+            </div>
+        </div>
     )
 }
