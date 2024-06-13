eadddf5ade2a40cc9d21ee7e98c13957db146d3b
Sunti <saknsaza87@gmail.com>
2024-06-13 15:30:20 +0700

    ออกจากระบบ

--- components/Button/ButtonWhiteBorderGray.js
+++ components/Button/ButtonWhiteBorderGray.js
@@ -1,6 +1,6 @@
 
-export default function ButtonWhiteBorderGray({ children }) {
-    return <div className="flex h-[32px] justify-center items-center w-[120px]] border border-1 rounded-3xl relative p-1 text-lg">
+export default function ButtonWhiteBorderGray({ children , height = "32px" }) {
+    return <div className={`flex h-[${height}] justify-center items-center w-[120px]] border border-1 rounded-3xl relative p-1 text-lg`}>
         {children}
     </div>
 }
