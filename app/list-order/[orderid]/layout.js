ecd845d7107603a32be931ab61e7d38d96ccb024
Sunti <saknsaza87@gmail.com>
2024-06-17 13:06:05 +0700

    optimize image

--- app/list-order/[orderid]/layout.js
+++ app/list-order/[orderid]/layout.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 export default function Layout({ children }) {
     return (
-        <div className="h-full w-full">
+        <div className="h-full w-full block">
             <Head>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
                 <link rel="subway" href="/favicon.ico" />
