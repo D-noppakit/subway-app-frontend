280a75077308f5e0df5975c2c15fe5d2fa86a89e
noppakit dononbout <d.noppakit@gmail.com>
2024-06-18 13:05:33 +0700

    Update subwaytextbox.js

--- components/subwaytextbox.js
+++ components/subwaytextbox.js
@@ -15,7 +15,7 @@ export default function Subwaytextbox({
           id=""
           className={
             "bg-gray-50 border border-gray-300 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 rounded-full " +
-            type
+            type ? type:''
           }
           style={{ paddingRight: "30px", width: "240px", height: "48px" }}
           disabled={type == 'disabled' ? true : null}
