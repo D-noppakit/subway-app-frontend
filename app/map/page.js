af07fbd683b6ec71f6db9f4f8c265d2cacdef43b
Sunti <saknsaza87@gmail.com>
2024-06-19 18:42:35 +0700

    map mock

--- app/map/page.js
+++ app/map/page.js
@@ -1,11 +1,16 @@
+"use client"
 import ButtonCustom from '@/components/Button/ButtonCustom';
 import CardBGWhite from '@/components/Card/CardBGWhite';
-import React from 'react';
+import React, { useState } from 'react';
 import Store1 from "@/public/icon/Store-1.png"
 import information from "@/public/icon/information-circle--information-frame-info-more-help-point-circle.png"
 import Image from 'next/image';
+import CardHeaderGreen from '@/components/Card/CardHeaderGreen';
+import ImagePhone from "@/public/icon/phone.png"
+import ImageMap1 from "@/public/icon/map1.png"
 
 export default function Map() {
+    const [closeInfo, setCloseInfo] = useState(false)
     const mock = Array.from({ length: 10 }, (_, index) => "1")
     return (
         <div className='h-screen w-screen'>
@@ -18,32 +23,62 @@ export default function Map() {
                     </div>
                 </div>
                 <div className="flex flex-col overflow-y-auto max-h-[60vh] hide-scrollbar ">
+
                     {mock.map((_, index) => {
                         return <CardBGWhite key={index}>
-                            <div className='flex items-center justify-between'>
-                                <div className='flex items-center' >
-                                    <div className='me-2'><Image src={Store1} width={16} height={16} /></div>
-                                    <div>ซับเวย์ แม็กซ์แวลูพัฒนาการ </div>
+                            <div onClick={() => setCloseInfo(true) }>
+                                <div className='flex items-center justify-between'>
+                                    <div className='flex items-center' >
+                                        <div className='me-2'><Image src={Store1} width={16} height={16} alt='icon-home' /></div>
+                                        <div>ซับเวย์ แม็กซ์แวลูพัฒนาการ </div>
+                                    </div>
+                                    <div className='flex self-start'>
+                                        <Image src={information} width={16} height={16} alt='image information' />
+                                    </div>
                                 </div>
-                                <div className='flex self-start'>
-                                    <Image src={information} width={16} height={16} />
+                                <div className='text-[12px] text-[#72747D] my-3'>
+                                    แม็กซ์แวลู, 255 ทางหลวงพิเศษหมาย เลข 7 แขวงสวนหลวง เขตสวนหลวง กรุงเทพมหานคร 10250
                                 </div>
-                            </div>
-                            <div className='text-[12px] text-[#72747D] my-3'>
-                                แม็กซ์แวลู, 255 ทางหลวงพิเศษหมาย เลข 7 แขวงสวนหลวง เขตสวนหลวง กรุงเทพมหานคร 10250
-                            </div>
-                            <div className='flex w-full justify-between items-center'>
-                                <div className='flex'>
-                                    <div className=' px-2 bg-[#97D700] text-white rounded-2xl me-3'>เปิด</div>
-                                    <div className='text-[#A3A6B7] text-[14px] flex justify-center items-center'> ปิดเวลา 19:30 น.</div>
+                                <div className='flex w-full justify-between items-center'>
+                                    <div className='flex'>
+                                        <div className=' px-2 bg-[#97D700] text-white rounded-2xl me-3'>เปิด</div>
+                                        <div className='text-[#A3A6B7] text-[14px] flex justify-center items-center'> ปิดเวลา 19:30 น.</div>
+                                    </div>
+                                    <div>0.10 กม.</div>
                                 </div>
-                                <div>0.10 กม.</div>
                             </div>
                         </CardBGWhite>
                     })}
 
                 </div>
             </div>
+            {closeInfo && <CardHeaderGreen close={()=> setCloseInfo(false)} titleHeader={"ซับเวย์ แม็กซ์แวลูพัฒนาการ "}>
+                <div className='flex gap-2 flex-col'>
+                    <div className='flex'>
+                        <div className=' px-2 bg-[#97D700] text-white rounded-2xl me-3'>เปิด</div>
+                    </div>
+                    <div className='text-[#72747D] text-[12px]'>
+                        แม็กซ์แวลู, 255 ทางหลวงพิเศษหมาย เลข 7 แขวงสวนหลวง เขตสวนหลวง กรุงเทพมหานคร 10250
+                    </div>
+                    <div className='text-[12px]'>0.10 กม.</div>
+                    <div className='text-[12px]'>ทุกวัน 7:00 - 19:30 น.</div>
+                    <div className='flex justify-between'>
+                        <div className='text-[#008938] text-[14px] flex'>
+                            <div className='me-1'>
+                                <Image src={ImagePhone} height={18} width={18} alt='phoneimage' />
+                            </div>
+                            <div></div>
+                            ติดต่อร้าน
+                        </div>
+                        <div className='text-[14px]'>02-343-4322</div>
+                    </div>
+                    <button className='text-[16px] text-[#008938] flex justify-center items-center bg-[#F8F9FB] w-full h-[56px] rounded-3xl'>
+                        <Image className='me-2' src={ImageMap1} width={24} height={24} alt='map' />
+                        <span>ดูแผนที่</span>
+                    </button>
+                </div>
+            </CardHeaderGreen>}
+
         </div>
     );
 }
