64c7e985f10ab1282cd6c2031411ebc6ea202bfc
KKTNW750 <karnkrit.tnw@gmail.com>
2024-06-19 12:54:11 +0700

    Update page.js

--- app/checkout/page.js
+++ app/checkout/page.js
@@ -1,17 +1,21 @@
 "use client"
+import CardSquare from "@/components/Card/CardSquare";
 import HeaderOne from "@/components/HeaderOne";
 import LocationAt from "@/components/HomeComponents/LocationAt";
 import Image from "next/image";
 import { useState } from "react";
-
 
 export default function Home2() {
     const [isOpenBulgur, setIsOpenBulgur] = useState(false)
     const ClickBulgur = (state) => {
         setIsOpenBulgur(state)
     }
-   return (
-      <div>
+    const OPTIONAL_RECOMMEND_MENU = []
+    for (let i = 0; i < 10; i++) {
+        OPTIONAL_RECOMMEND_MENU.push(<CardSquare fullPrice={185} key={'orm_' + i}/>)
+    }
+    return (
+        <div>
             <HeaderOne CartCount={0} whenClickBulgur={ClickBulgur} />
             <div style={{ height: "fit-content" }}>
                 <main className={`w-full h-[139px] bg-[#0b8a45] relative`}>
@@ -26,6 +30,15 @@ export default function Home2() {
 
                 </main>
             </div>
-      </div>
-   );
+            <div className="w-screen p-5">
+                <div className="h-[385px]"></div>
+            </div>
+            <div className="h-[215px] pl-5 gap-[5px] flex flex-col">
+                <span className="text-[#0C8A44] text-16px font-bold">สั่งเพิ่มรับรองว่าถูกใจ</span>
+                <div className="flex h-[175px] gap-[12px] overflow-x-scroll w-[680px]">
+                   {OPTIONAL_RECOMMEND_MENU}
+                </div>
+            </div>
+        </div>
+    );
 }
