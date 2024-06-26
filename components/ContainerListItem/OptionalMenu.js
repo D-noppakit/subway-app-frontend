218b1b2431f14a49538cae6e128607095d41d210
KKTNW750 <karnkrit.tnw@gmail.com>
2024-06-26 18:20:16 +0700

    checkout

---
+++ components/ContainerListItem/OptionalMenu.js
@@ -0,0 +1,22 @@
+'use client'
+import React from 'react'
+import CardSquare from '../Card/CardSquare'
+export default function OptionalMenu() {
+    const OPTIONAL_RECOMMEND_MENU = []
+    for (let i = 0; i < 10; i++) {
+        OPTIONAL_RECOMMEND_MENU.push(
+            <div className="w-[120px] flex">
+                <CardSquare fullPrice={185} key={'orm_' + i} width={"120px"}/>
+            </div>
+        )
+    }
+    return (
+        <div className="h-[215px] pl-5 gap-[5px] flex flex-col mb-[15px]">
+            <span className="text-[#0C8A44] text-16px font-bold">สั่งเพิ่มรับรองว่าถูกใจ</span>
+            <div className="flex h-[175px] gap-[12px] overflow-x-scroll w-full pr-5">
+                {OPTIONAL_RECOMMEND_MENU}
+            </div>
+        </div>
+    )
+}
+
